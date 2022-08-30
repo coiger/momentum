60e06677b0cb286c05f72a492680fcb93639ec04
Hochan Kang <kanghc0217@naver.com>
2022-08-30 12:43:07 +0900

    feat: Cache username

--- app.mjs
+++ app.mjs
@@ -1,13 +1,22 @@
 const HIDDEN_CLASS = 'hidden';
+const USERNAME_KEY = 'username';
 
 const $loginForm = document.getElementById('login');
 const $greeting = document.getElementById('greeting');
+
+const username = localStorage.getItem(USERNAME_KEY);
+if (username) {
+  $greeting.textContent = `Hello ${username}`;
+  $loginForm.classList.add(HIDDEN_CLASS);
+  $greeting.classList.remove(HIDDEN_CLASS);
+}
 
 $loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const username = e.target.firstElementChild.value;
   $greeting.textContent = `Hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
 
   $loginForm.classList.add(HIDDEN_CLASS);
   $greeting.classList.remove(HIDDEN_CLASS);
