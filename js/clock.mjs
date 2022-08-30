bb8ff152a2ef7f2c27d0c7e6770041eb6f5a1ce0
Hochan Kang <kanghc0217@naver.com>
2022-08-30 17:30:36 +0900

    docs: Describe paintClock func

--- js/clock.mjs
+++ js/clock.mjs
@@ -1,5 +1,8 @@
 const $clock = document.getElementById('clock');
 
+/**
+ * Paint current time (hh:mm:ss) on screen
+ */
 function paintClock() {
   const date = new Date();
   $clock.textContent = [date.getHours(), date.getMinutes(), date.getSeconds()]
