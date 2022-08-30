6f168317db520efe3f29a8c61bb55ae1f4f5f0e5
Hochan Kang <kanghc0217@naver.com>
2022-08-30 12:45:29 +0900

    refactor: Extract codes to function

--- app.mjs
+++ app.mjs
@@ -4,20 +4,19 @@ const USERNAME_KEY = 'username';
 const $loginForm = document.getElementById('login');
 const $greeting = document.getElementById('greeting');
 
-const username = localStorage.getItem(USERNAME_KEY);
-if (username) {
+function paintGreetings(username) {
   $greeting.textContent = `Hello ${username}`;
   $loginForm.classList.add(HIDDEN_CLASS);
   $greeting.classList.remove(HIDDEN_CLASS);
 }
 
+const username = localStorage.getItem(USERNAME_KEY);
+if (username) paintGreetings(username);
+
 $loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const username = e.target.firstElementChild.value;
-  $greeting.textContent = `Hello ${username}`;
   localStorage.setItem(USERNAME_KEY, username);
-
-  $loginForm.classList.add(HIDDEN_CLASS);
-  $greeting.classList.remove(HIDDEN_CLASS);
+  paintGreetings(username);
 });
